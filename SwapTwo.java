38565fb23b0ce2e90754f271944237f27152949b
36raj55 <138805285+36raj55@users.noreply.github.com>
2023-08-01 20:31:42 +0530

    Add files via upload

---
+++ SwapTwo.java
@@ -0,0 +1,16 @@
+package corejava;
+
+public class SwapTwo {
+
+	public static void main(String[] args) {
+		int a=10,b=20;
+		int temp;
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println("a" +a);
+		System.out.println("b" +b);
+
+	}
+
+}
