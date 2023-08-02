019c890fb1a6dba078b38cbf54c28bb5e528e193
36raj55 <138805285+36raj55@users.noreply.github.com>
2023-08-02 10:00:34 +0530

    Add files via upload

---
+++ SortAscendingOrder.java
@@ -0,0 +1,32 @@
+
+public class SortAscendingOrder {
+
+	public static void main(String[] args) {
+          int[]arr = new int[]{85,25,65,35,55,13,47};
+          int temp=0;
+          System.out.println("orginal array");
+          for(int i=0;i<arr.length;i++) {
+        	  System.out.println(arr[i]+"");
+          }
+          for(int i=0;i<arr.length;i++) {
+        	  for(int j=i+1;j<arr.length;j++) {
+        		  if(arr[i]>arr[j]) {
+        			  temp = arr[i];
+        			  arr[i] = arr[j];
+        			  arr[j] = temp;
+        			   
+        			  System.out.println();
+        			  
+        			
+        		  }
+        	  }
+          }
+	     
+          System.out.println(" array sorted in ascending order");
+          for(int i=0;i<arr.length;i++) {
+        	  System.out.println(arr[i]+"");
+          }
+	}
+ 
+	
+}
