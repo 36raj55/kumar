b38438338461e306ddb7762811f3189dedd7e8e0
36raj55 <138805285+36raj55@users.noreply.github.com>
2023-08-02 10:01:25 +0530

    Add files via upload

---
+++ StringLength.java
@@ -0,0 +1,11 @@
+
+public class StringLength {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String txt = "gokavarapuvenkateswararao";
+		System.out.println("the length of the txt string is: " + txt.length());
+
+	}
+
+}
