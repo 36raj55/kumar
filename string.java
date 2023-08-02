85c7a36c21e0db0adc02efbb14a908db3193778e
36raj55 <138805285+36raj55@users.noreply.github.com>
2023-08-02 09:50:56 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,13 @@
+package corejava;
+
+public class string {
+	public String breed="German Shepard";
+	public int height=30;
+	public int weight=45;
+
+	public static void main(String[] args) {
+	System.out.println("test");
+
+	}
+
+}
